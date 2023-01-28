2d1e072a046ef43a214c3207cafe63d556cf820f
ReshiNagarajan <reshinina21@gmail.com>
2023-01-28 06:37:19 +0530

    new data

---
+++ src/org/dataswapping/DataSwapping.java
@@ -0,0 +1,29 @@
+package org.dataswapping;
+
+import java.util.Arrays;
+
+public class DataSwapping {
+	
+	public static void main(String[]args) {
+		String b=null;
+		
+		String[] a= {"Reshi","Rajesh","Ramesh","Suresh","zaya","Bonvita"};
+		for(String b1:a) {System.out.println(b1);}
+	//	System.out.println('\n');
+		Arrays.sort(a);
+		for(String c:a)// {System.out.println(c);
+	
+ b=c;
+
+	
+System.out.println(b);
+
+
+
+
+
+
+
+
+
+}}
